2b6c5974675e040c55ec28bd2efaef176772b67f
Astra0909778 <j13131381@gmail.com>
2025-03-23 15:26:04 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+MONGO_URI = "mongodb+srv://umeshtheboss355:<PSNJS1QGQDuMkOM0>@cluster0.ktcud.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+API_ID = "27948006"
+API_HASH = "3cee2dae78c3f70b1d60409d7ae0b698"
+BOT_TOKEN = "7600673250:AAHrBMtOPJcBnFrU3CzPafJ-i2FCJpWEWNA"
+ADMIN_ID = 2001661821   # Apna Telegram ID dal
+ADMIN_UPI_ID = "mukeshseller2@fam"
